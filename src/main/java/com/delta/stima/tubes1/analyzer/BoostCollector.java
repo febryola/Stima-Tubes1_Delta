595239355e0ef72779795caefd1a16385ecf1db5
bayusamudra5502 <contact@bayusamudra.my.id>
2022-02-12 23:57:44 +0700

    Memberi contoh implementasinya

--- src/main/java/com/delta/stima/tubes1/analyzer/BoostCollector.java
+++ src/main/java/com/delta/stima/tubes1/analyzer/BoostCollector.java
@@ -1,5 +1,6 @@
 package com.delta.stima.tubes1.analyzer;
 
+import com.delta.stima.tubes1.command.DoNothingCommand;
 import com.delta.stima.tubes1.entities.GameState;
 
 public class BoostCollector extends BaseAnalyzer {
@@ -8,9 +9,9 @@ public class BoostCollector extends BaseAnalyzer {
     }
 
     public void analyze(){
-        // ...
+        // Tentukan kondisinya gimana
         if(kondisi){
-            this.setSolution(...);
+            this.setSolution(new DoNothingCommand()); // Klo emang memenuhi syarat, pasang solusi
         }
     }
 }
