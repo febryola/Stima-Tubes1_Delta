e63e9c8872283e1558925be760549e8c35db942b
bayusamudra5502 <contact@bayusamudra.my.id>
2022-02-12 12:14:15 +0700

    Menambahkan test

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ all:
 	@mvn install
 	@mv bin/Delta-jar-with-dependencies.jar bin/Delta.jar
 
-.PHONY: all
+test:
+	@mvn test
+
+.PHONY: all test
---
+++ test/com/delta/stima/tubes1/controller/ControllerTest.java
@@ -0,0 +1,11 @@
+package com.delta.stima.tubes1.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ControllerTest {
+    @Test
+    public void testController(){
+        Assertions.assertTrue(true);
+    }
+}
