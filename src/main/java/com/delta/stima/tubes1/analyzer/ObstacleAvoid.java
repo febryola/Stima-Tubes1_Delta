303cc81e79ec137220e28221e14f9db88b2a26b9
bayusamudra5502 <contact@bayusamudra.my.id>
2022-02-14 21:44:49 +0700

    Membuat steer lebih jelas

--- src/main/java/com/delta/stima/tubes1/analyzer/ObstacleAvoid.java
+++ src/main/java/com/delta/stima/tubes1/analyzer/ObstacleAvoid.java
@@ -2,10 +2,12 @@ package com.delta.stima.tubes1.analyzer;
 
 import com.delta.stima.tubes1.command.ChangeLaneCommand;
 import com.delta.stima.tubes1.command.DecelerateCommand;
+import com.delta.stima.tubes1.command.FixCommand;
 import com.delta.stima.tubes1.command.LizardCommand;
 import com.delta.stima.tubes1.entities.GameState;
 import com.delta.stima.tubes1.entities.Lane;
 import com.delta.stima.tubes1.enums.PowerUps;
+import com.delta.stima.tubes1.enums.SteerDirection;
 import com.delta.stima.tubes1.enums.Terrain;
 
 public class ObstacleAvoid extends BaseAnalyzer {
@@ -70,7 +72,7 @@ public class ObstacleAvoid extends BaseAnalyzer {
 
         // Cek Kiri ada obstacle ga?
         if(currentLane > 1 && !this.isObstacleExist(currentLane-1)){
-            this.setSolution(new ChangeLaneCommand(-1));
+            this.setSolution(new ChangeLaneCommand(SteerDirection.LEFT));
             return;
         }
 
--- src/main/java/com/delta/stima/tubes1/command/ChangeLaneCommand.java
+++ src/main/java/com/delta/stima/tubes1/command/ChangeLaneCommand.java
@@ -1,13 +1,14 @@
 package com.delta.stima.tubes1.command;
 
 import com.delta.stima.tubes1.enums.Direction;
+import com.delta.stima.tubes1.enums.SteerDirection;
 
 public class ChangeLaneCommand implements Command {
 
     private Direction direction;
 
-    public ChangeLaneCommand(int laneIndicator) {
-        if (laneIndicator == 1) {
+    public ChangeLaneCommand(SteerDirection position) {
+        if (position == SteerDirection.RIGHT) {
             this.direction = Direction.valueOf("RIGHT");
         } else {
             this.direction = Direction.valueOf("LEFT");
