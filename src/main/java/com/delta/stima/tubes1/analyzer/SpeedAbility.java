f54d94c90ea6b199a9fa140fcc6359d20ce34c74
febryola <febryola0402@gmail.com>
2022-02-15 23:43:07 +0700

    updateeee

--- src/main/java/com/delta/stima/tubes1/analyzer/SpeedAbility.java
+++ src/main/java/com/delta/stima/tubes1/analyzer/SpeedAbility.java
@@ -18,29 +18,20 @@ public class SpeedAbility extends BaseAnalyzer{
 
         return false;
     }
-<<<<<<< HEAD
 
     public boolean checkKondisiAmanJikaSpeed(){
         int playerBlock = this.playerCar.position.block;
         int opponentBlock = this.opponentCar.position.block;
         for(PowerUps ps: this.gameState.player.powerups) {
-            if(ps == PowerUps.BOOST){
-                if((opponentBlock-playerBlock>=15)) {
+            if (ps == PowerUps.BOOST) {
+                if ((opponentBlock - playerBlock >= 15)) {
                     return true;
                 }
-=======
-    public void analyze(){
-        if(this.checkIsBoostExist()){
-            this.setSolution(new BoostCommand());
-            return;
-        }
->>>>>>> 604eeb13e6eee2f4e206b1f96952aa2346885d18
-
             }
         }
-
         return false;
     }
+
     public void analyze(){
         if(this.checkIsBoostExist()){
             if(this.checkKondisiAmanJikaSpeed()) {
