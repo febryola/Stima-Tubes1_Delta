7387236a7c1f8610ad8d1df61bb9663738abbe58
bayusamudra5502 <contact@bayusamudra.my.id>
2022-02-16 20:08:38 +0700

    Memperbaiki powerups

--- src/main/java/com/delta/stima/tubes1/analyzer/PowerupsCollector.java
+++ src/main/java/com/delta/stima/tubes1/analyzer/PowerupsCollector.java
@@ -9,12 +9,12 @@ import java.lang.Math;
 import java.util.List;
 
 public class PowerupsCollector extends BaseAnalyzer {
-    public PowerupsCollector(GameState gameState){
+    public PowerupsCollector(GameState gameState) {
         super(gameState);
     }
 
     private void setCommandResult(int pos) {
-        switch (pos){
+        switch (pos) {
             case 1:
                 this.setSolution(new ChangeLaneCommand(SteerDirection.LEFT));
                 break;
@@ -24,21 +24,45 @@ public class PowerupsCollector extends BaseAnalyzer {
         }
     }
 
-    public void analyze(){
+    private int[] LaneDamage() {
+        int[] result = new int[3];
+        int lane = this.playerCar.position.lane;
+        int speed = this.playerCar.speed;
+
+        result[0] = this.totalDamage(lane, speed);
+
+        if (lane > 1) {
+            result[1] = this.totalDamage(lane - 1, speed);
+        } else {
+            result[1] = Integer.MAX_VALUE;
+        }
+
+        if (lane < this.gameState.lanes.size()) {
+            result[2] = this.totalDamage(lane + 1, speed);
+        } else {
+            result[2] = Integer.MAX_VALUE;
+        }
+
+        return result;
+    }
+
+    public void analyze() {
         List<List<Terrain>> l = this.getLanes();
 
         final Terrain[] powerups = {
-           Terrain.BOOST,
-           Terrain.LIZARD,
-           Terrain.TWEET,
-           Terrain.EMP,
-           Terrain.OIL_POWER
+                Terrain.BOOST,
+                Terrain.LIZARD,
+                Terrain.TWEET,
+                Terrain.EMP,
+                Terrain.OIL_POWER
         };
 
-        for(Terrain type: powerups) {
-            for(int i = 0; i < l.size(); i++){
-                for(int j = 0; j < Math.min(this.playerCar.speed + 1, l.get(i).size()); j++){
-                    if(l.get(i).get(j) == type){
+        final int[] damage = LaneDamage();
+
+        for (Terrain type : powerups) {
+            for (int i = 0; i < l.size(); i++) {
+                for (int j = 0; j < Math.min(this.playerCar.speed + 1, l.get(i).size()); j++) {
+                    if (l.get(i).get(j) == type && (damage[i] < 2 || i == 0)) {
                         this.setCommandResult(i);
                         return;
                     }
